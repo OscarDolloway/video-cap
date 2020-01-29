48432493a34e138d70874dd9c338d2f58c8bec61
Oscar Home Computer <oscar.dolloway@hotmail.co.uk>
2020-01-29 11:33:56 +0000

    test

---
+++ syscommands.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 11:29:54 2020
+
+@author: oscardolloway
+"""
+
+
+import os
+
+
+
+directory = (os.path.abspath(os.path.dirname(sys.argv[0])))
+
+ffmpeg = directory + '/bin/ffmpeg'
+ffprobe = directory + '/bin/ffprobe'
+command1 = [ffprobe,'http://www.bokowsky.net/de/knowledge-base/video/hls_bunny/7ea96983ed10dc5546f8275871a38df7_127912_60594786.m3u8']
+
+
+os.system(ffmpeg)
+
+
+
+#cmd = [ffmpeg] + '-codecs'
+
+#os.system(ffprobe+'http://www.bokowsky.net/de/knowledge-base/video/hls_bunny/7ea96983ed10dc5546f8275871a38df7_127912_60594786.m3u8')
+
+s
