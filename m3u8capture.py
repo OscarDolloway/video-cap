6f39375a2d74f71456c67418d9ec7123b69ac299
Oscar Work <odolloway@friendmts.com>
2020-02-03 09:47:40 +0000

    Revert "Revert "Merge branch 'master' of https://github.com/OscarDolloway/video-cap""
    This reverts commit 669203af76686a4ec210b2f8c475016070fea159.

--- m3u8capture.py
+++ m3u8capture.py
@@ -6,63 +6,46 @@ Created on Sun Jan 26 14:41:32 2020
 
 @author: oscardolloway
 """
-from imports import *   
-
-
 #print(viddir)
+from imports import *
 
 
 #URL = 'http://95.170.215.120/hls/m3u8/BT-Sport-1HD.m3u8'
+print('----- Enter site for capture:')
+URL = input ('---- : ')
+print('\n')
 
-URL = input ('Enter site: ')
-
-
-StrURL = (", ".join(URL))
+#StrURL = (", ".join(URL))
 URLlist = list(URL.split(" "))
-print(type(URLlist))
+#print(type(URLlist))
 
 
 # =============================================================================
-# Step one check URL, if URL is M3U8 then run capture function, else run scraper
+# check URL status
 # =============================================================================
-
-
+# check what URL m3u8 or HTML
+# =============================================================================
+#           if m3u8, attempt a capture
+#       else
+#            attempt a scrape of links from the site
+#scrape the multiple links
+# =============================================================================
 
 def m3u8(URL):
     if webcheck(URL) == True:
-        print('webcheck, site up')
+        print('---- Webcheck complete, site is online'+ '\n')
         StrURL = (", ".join(URL))
         if 'm3u8' in StrURL:
             print('link contains m3u8','beginning stream capture')
             single_Capture(StrURL)
         else:
-            print('working site but not .m3u8, running web scraper..')
+            print('---- Does not contain m3u8, running web scraper')
             if m3u8scraper(StrURL) == True:
                 pass
+                #not quite ready
                 #Capture(linkset)
-
 if type(URLlist) is list:
     m3u8(URLlist)
-
-     
-#print(multilinks)
-#URL = multilinks
-#print(URL)
-        
-
-
-        #for i in linkset:
-          #  print(i)
-        #print(type(linkset))
-        
-
-#print(webcheck(URL))
-
-#m3u8scraper(URL)
-
-#Capture(URL)
-
-
 
         
 
